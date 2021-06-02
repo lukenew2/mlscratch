4f09d9c4a6705e0345a2b52f328d2c0e6f212789
Luke Newman <luken2@yahoo.com>
2021-06-02 11:02:41 -0700

    edit setup.py and track regularization.py

---
+++ mlscratch/utils/regularization.py
@@ -0,0 +1,140 @@
+"""Module containing classes for regularization of linear models."""
+import numpy as np
+
+class l1_regularization():
+    """
+    Add l1 regularization penalty to linear models.
+
+    Regularization term:
+
+        alpha * ||w||
+
+    where w is the vector of feature weights and alpha is the hyperparameter
+    controlling how much regularization is done to the model.
+
+    Parameters
+    ----------
+    alpha : float, default=1.0
+        Factor determining the amount of regularization to be performed on
+        the model.
+
+    Notes
+    -----
+    The bias term is not regularized and therefore should be omitted from the
+    feature weights as input.  
+    """
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha 
+
+    def __call__(self, w):
+        "Calculate l1 regularization term."
+        return self.alpha * np.linalg.norm(w, 1)
+
+    def grad(self, w):
+        """Calculate subgradient vector of l1 regularization penalty.
+        
+                      -1 if w_i < 0
+            sign(w) =  0 if w_i = 0
+                       1 if w_i > 0
+
+        where w is the vector of feature weights.
+        """
+        subgradient = self.alpha * np.sign(w)
+        # Insert 0 for bias term.
+        return np.insert(subgradient, 0, 0, axis=0)
+
+class l2_regularization():
+    """
+    Add l2 regularization penalty to linear models.
+
+    Regularization term:
+
+        alpha * 1/2 * ||w||^2
+
+    Where w is the vector of feature weights and alpha is the hyperparameter
+    controlling how much regularization is done to the model.
+
+    Parameters
+    ----------
+    alpha : float, default=1.0
+        Factor determining the amount of regularization to be performed on
+        the model.
+
+    Notes
+    -----
+    The bias term is not regularized and therefore should be omitted from the
+    feature weights as input.  
+    """
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha 
+
+    def __call__(self, w):
+        """Calculate regularization term."""
+        return self.alpha * 0.5 * np.linalg.norm(w, 2)
+    
+    def grad(self, w):
+        """
+        Calculate gradient descent regularization term.
+
+            alpha * w
+
+        where alpha is the factor determining the amount of regularization and
+        w is the vector of feature weights.  
+        """
+        gradient_penalty = np.asarray(self.alpha) * w
+        # Insert 0 for bias term.
+        return np.insert(gradient_penalty, 0, 0, axis=0)
+
+
+class l1_l2_regularization():
+    """
+    Add a mix of l1 and l2 regularization penalty to linear models.
+
+    Regularization term:
+
+        r * alpha * ||w|| + (1 - r) / 2 * alpha * ||w||^2
+
+    where r is the mix ratio, alpha is the factor determining the amount
+    of regularization and w is the vector of feature weights.
+
+    Parameters
+    ----------
+    alpha : float, default=1.0
+        Factor determining the amount of regularization to be performed on
+        the model.
+    r : float, default=0.5
+        Mix ratio determining the amount of l1 vs l2 regularization to add.  
+        A value of 0 is equivalent to l2 regularization and a value of 1 is
+        equivalent to l1 regularization.
+
+    Notes
+    -----
+    The bias term is not regularized and therefore should be omitted from the
+    feature weights as input.  
+    """
+    def __init__(self, alpha=1.0, r=0.5):
+        self.alpha = alpha
+        self.r = r 
+
+    def __call__(self, w):
+        """Calculate elastic net regularization penalty."""
+        l1_term = self.alpha * np.linalg.norm(w, 1)
+        l2_term = self.alpha * 0.5 * np.linalg.norm(w, 2)
+
+        return self.r * l1_term + (1 - self.r) * l2_term
+
+    def grad(self, w):
+        """
+        Calculate gradient descent regularization penalty.
+
+            alpha * (r * sign(w) + (1 - r) * w)
+
+        where r is the mix ratio, alpha is the factor determining the amount
+        of regularization and w is the vector of feature weights.
+        """
+        l1_grad = self.r * np.sign(w)
+        l2_grad = np.asarray(1 - self.r) * w 
+
+        gradient_penalty = self.alpha * (l1_grad + l2_grad)
+        # Insert 0 for bias term.
+        return np.insert(gradient_penalty, 0, 0, axis=0)
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_namespace_packages
+import pathlib
+
+__version__ = '0.0.1'
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='mlscratch',  
+    version=__version__,  
+    description='Machine learning from scratch', 
+    long_description=long_description,  
+    long_description_content_type='text/markdown',  
+    url='https://github.com/lukenew2/mlscratch', 
+    author='Luke Newman', 
+    author_email='lukeanewman2@gmail.com',  
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='data science, machine learning',  
+    packages=find_namespace_packages(include='mlscratch'), 
+    python_requires='>=3.7',
+    install_requires=[
+        'scipy',
+        'numpy'
+    ],  
+    project_urls={  
+        'Source': 'https://github.com/lukenew2/mlscratch',
+    },
+)
